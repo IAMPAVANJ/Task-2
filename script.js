ea4529e3c617e73698feca4fec0831cc7cb7d0cc
Pavan Jadhav <114083627+IAMPAVANJ@users.noreply.github.com>
2023-03-17 22:58:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,23 @@
 const changeColorButton = document.getElementById("color-btn");
 const childAddButton = document.getElementById('addChild-btn');
 const childRemoveButton = document.getElementById("removeChild-btn");
+let count = 0;
 changeColorButton.addEventListener("click",function(){
     let n1 = Math.floor(Math.random()*100)
-    let n2 = Math.floor(Math.random()*100)
-    let n3 = Math.floor(Math.random()*100)
-    let n4 = Math.floor(Math.random()*100)
-    document.getElementById('container').setAttribute("style",`background-color: rgba(${n4}, ${n3}, ${n2}, ${n1})`)
-
+    let n2 = Math.floor(Math.random()*20)
+    let n3 = Math.floor(Math.random()*15)
+    document.getElementById('container').setAttribute("style",`background-color: rgb(${n1}, ${n2}, ${n3}`)
 })
 
 
 
 childAddButton.addEventListener("click",function(){
-    document.getElementById("child-container").innerHTML += `<div id="childDiv"></div>`
+    count++;
+    document.getElementById("child-container").innerHTML += `<div id="childDiv">${count}</div>`
 })
 
 childRemoveButton.addEventListener("click",function(){
+    count--;
   let parent =  document.getElementById("child-container");
   parent.removeChild(parent.lastChild)
   
